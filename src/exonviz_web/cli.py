6648a625f551d6643970574fe05671626e617720
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2024-02-12 07:54:43 +0100

    Link Flask logging with gunicorn

--- src/exonviz_web/cli.py
+++ src/exonviz_web/cli.py
@@ -4,11 +4,19 @@ without executing side effects
 """
 
 from .app import app
+import logging
 
 
 def main() -> None:
     # Pull FLASK_SECRET_KEY from environment, or use fallback random ID
     app.config.from_prefixed_env()
+
+    # Tie the flask log into gunicorn
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
+    # Log the secret key
     app.logger.info(f"Secret key: {app.config['SECRET_KEY']}")
     app.run()
 

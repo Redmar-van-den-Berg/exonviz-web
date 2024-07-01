8383331db759a1b3a9bc49daa924d62a5ebf06ee
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2024-07-01 13:52:37 +0200

    Update ExonViz to 0.2.11

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     python_requires='>=3.9',
     install_requires=[
         'flask',
-        'exonviz>=0.2.9',
+        'exonviz>=0.2.11',
         'typing_extensions',
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
